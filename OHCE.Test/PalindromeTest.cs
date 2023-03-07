4b290b01099ecc42ae6034b9888b15d628c32cab
Enzo Sandré <atelier@enzosandre.fr>
2023-03-07 18:56:58 +0100

    Fusion de 2 tests

--- OHCE.Test/PalindromeTest.cs
+++ OHCE.Test/PalindromeTest.cs
@@ -17,18 +17,24 @@ public class PalindromeTest
         Assert.Contains(résultatAttendu, résultat);
     }
 
-    [Fact]
-    public void BienDitFRTest()
+    public static IEnumerable<object[]> Langues => new[]
     {
-        // ETANT DONNE un utilisateur parlant français
-        var langue = new LangueFrançaise();
+        new object[] { new LangueAnglaise() },
+        new object[] { new LangueFrançaise() }
+    };
+
+    [Theory]
+    [MemberData(nameof(Langues))]
+    public void BienDitTest(ILangue langue)
+    {
+        // ETANT DONNE un utilisateur parlant <langue>
+        var ohce = new DétectionPalindromeBuilder()
+            .AyantPourLangue(langue)
+            .Build();
 
         // QUAND on saisit un palindrome
         const string palindrome = "radar";
-        var résultat = new DétectionPalindromeBuilder()
-            .AyantPourLangue(langue)
-            .Build()
-            .TraiterChaîne(palindrome);
+        var résultat = ohce.TraiterChaîne(palindrome);
 
         // ALORS celui-ci est renvoyé
         Assert.Contains(palindrome, résultat);
@@ -38,30 +44,6 @@ public class PalindromeTest
         var résultatAprèsPalindrome = résultat[finPalindrome..];
 
         // ET le « Bien dit » de cette langue est envoyé ensuite
-        Assert.StartsWith(langue.Félicitations, résultatAprèsPalindrome);
-    }
-
-    [Fact]
-    public void BienDitENTest()
-    {
-        // ETANT DONNE un utilisateur parlant anglais
-        var langue = new LangueAnglaise();
-
-        // QUAND on saisit un palindrome
-        const string palindrome = "radar";
-        var résultat = new DétectionPalindromeBuilder()
-            .AyantPourLangue(langue)
-            .Build()
-            .TraiterChaîne(palindrome);
-
-        // ALORS celui-ci est renvoyé
-        Assert.Contains(palindrome, résultat);
-
-        var premièreOccurrence = résultat.IndexOf(palindrome, StringComparison.Ordinal);
-        var finPalindrome = premièreOccurrence + palindrome.Length;
-        var résultatAprèsPalindrome = résultat[finPalindrome..];
-
-        // ET le « Bien dit »  de cette langue est envoyé ensuite
         Assert.StartsWith(langue.Félicitations, résultatAprèsPalindrome);
     }
 
